e3e7ddc54096916997f20068175d227f681653be
Jay.H.Zou <Jay.H.Zou@newegg.com>
2019-02-20 19:13:20 +0800

    init

---
+++ src/main/java/com/newegg/ec/tools/MonitorPlatformApplication.java
@@ -0,0 +1,17 @@
+package com.newegg.ec.tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jay.H.Zou
+ * @date 2019/2/20
+ */
+@SpringBootApplication
+public class MonitorPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorPlatformApplication.class, args);
+    }
+
+}
