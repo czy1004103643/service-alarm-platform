59b66082194820de19c334aa8d993b8d3c66dec9
lf52 <Leo.J.Fu@newegg.com>
2019-02-23 11:09:39 +0800

    update mode

---
+++ alarm-platform-component/src/test/java/com/newegg/ec/tools/component/wechat/WechatAlarmTest.java
@@ -0,0 +1,14 @@
+package com.newegg.ec.tools.component.wechat;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class WechatAlarmTest {
+
+    @Test
+    public void getAccessToken() throws IOException {
+        String accessToken = WechatAPI.getAccessToken();
+        System.err.println("\n" + accessToken);
+    }
+}
