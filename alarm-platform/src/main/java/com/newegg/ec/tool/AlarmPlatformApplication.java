02a77583c071569a48a01b92cc50bc4bac878f50
gz75 <newegg@456>
2019-02-26 13:44:04 +0800

    init

---
+++ alarm-platform/src/main/java/com/newegg/ec/tool/AlarmPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.newegg.ec.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlarmPlatformApplication.class, args);
+    }
+
+}
